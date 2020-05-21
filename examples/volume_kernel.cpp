0c3c9814da7d873632e64ceafc384fd5163a00d1
Stefan Groth <sgroth@ln-0002.cm.cluster>
2020-05-21 12:08:57 +0200

    measure kernel

--- examples/volume_kernel.cpp
+++ examples/volume_kernel.cpp
@@ -13,6 +13,8 @@ int main(int argc, char **argv)
 
   using DG = DgNodes<double, Vec3D, order>;
 
+  std::chrono::nanoseconds in_kernel { 0 };
+
   auto volume_kernel = HPM::ForEachEntity(
       e.AllCells,
       std::tuple(
@@ -21,8 +23,13 @@ int main(int argc, char **argv)
           ReadWrite(Cell(rhsH)),
           ReadWrite(Cell(rhsE))),
       [&](const auto &element, const auto &, auto &lvs) {
-        const Mat3D &D = element.GetGeometry().GetInverseJacobian() * 2.0;
+        
+	in_kernel += HPM::auxiliary::MeasureTime(
+                                                                     [&]() {
 
+
+	const Mat3D &D = element.GetGeometry().GetInverseJacobian() * 2.0;
+	
         HPM::ForEach(DG::numVolNodes, [&](const std::size_t n) {
           Mat3D derivative_E, derivative_H; //!< derivative of fields w.r.t reference coordinates
 
@@ -39,10 +46,15 @@ int main(int argc, char **argv)
 
           rhsH[n] += -Curl(D, derivative_E); //!< first half of right-hand-side of fields
           rhsE[n] += Curl(D, derivative_H);
+	
         });
-      });
+
+		});
+     
+});
 
   print_time(MeasureKernel(volume_kernel));
+  std::cout << "In Kernel = " << std::chrono::duration_cast<std::chrono::milliseconds>(in_kernel).count() << " ms\n";
 
   return EXIT_SUCCESS;
 }
