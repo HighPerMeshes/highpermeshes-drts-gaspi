53103c2a4a41a48db11a5b787c27a6887b3a1c35
Merlind Schotte <schotte@zib.de>
2020-06-04 16:33:00 +0200

    clean up functions and replace std::vector variables by bufferT variables

--- examples/Monodomain_DSL/src/monodomain2D.cpp
+++ examples/Monodomain_DSL/src/monodomain2D.cpp
@@ -18,7 +18,7 @@
  *                  u(0) = 1  on \Omega_1 and u(0) = 0  on \Omega_2,               *
  *                  w(0) = 0  on \Omega_1 and w(0) = 1  on \Omega_2.               *
  *                                                                                 *
- * last change: 29.05.2020                                                         *
+ * last change: 04.06.2020                                                         *
  * ------------------------------------------------------------------------------ **/
 
 #ifndef MONODOMAIN_CPP
@@ -34,6 +34,9 @@
 #include <../examples/Functions/outputWriter.hpp>
 #include <../examples/Functions/simplexGradients.hpp>
 
+#include <unistd.h>
+#define GetCurrentDir getcwd
+
 class DEBUG;
 using namespace HPM;
 using namespace ::HPM::auxiliary;
@@ -46,26 +49,25 @@ using Mesh           = HPM::mesh::PartitionedMesh<CoordinateType, HPM::entity::S
 constexpr int dim    = Mesh::CellDimension;
 
 /*-------------------------------------------------------------- (A) Functions: -------------------------------------------------------------------------------*/
+void SetStartValues(int option, float& h, float& a, float& b, float& eps, float& sigma, float& u0L, float& u0R, float& w0L, float& w0R);
+
 template<typename MeshT, typename BufferT, typename LoopBodyT>
-void CreateStartVector(const MeshT & mesh, BufferT & startVec, const float & startValLeft, const float & startValRight, const int & maxX, const int & maxY, LoopBodyT bodyObj);
+void CreateStartVector(const MeshT & mesh, BufferT & startVec, const float & startValLeft, const float & startValRight,
+                       const int & maxX, const int & maxY, LoopBodyT & body);
 
 template<typename MeshT, typename LoopBodyT, typename BufferT>
-void AssembleLumpedMassMatrix(const MeshT & mesh, LoopBodyT bodyObj, BufferT & lumpedMat) ;
+void AssembleLumpedMassMatrix(const MeshT & mesh, LoopBodyT & body, BufferT & lumpedMat) ;
 
 template<typename MeshT, typename VectorT, typename LoopbodyT, typename BufferT>
-void AssembleMatrixVecProduct2D(const MeshT & mesh, const VectorT & d, LoopbodyT bodyObj, BufferT & sBuffer);
+void AssembleMatrixVecProduct2D(const MeshT & mesh, const VectorT & d, LoopbodyT & body, BufferT & sBuffer);
 
 template<typename BufferT, typename VectorT, typename LoopbodyT, typename MeshT>
-void FWEuler(BufferT & vecOld, const VectorT & vecDeriv, const float & h, LoopbodyT body, const MeshT & mesh);
+void FWEuler(BufferT & vecOld, const VectorT & vecDeriv, const float & h, LoopbodyT & body, const MeshT & mesh);
 
-template<typename BufferT>
-auto Iion(const BufferT & u, const BufferT & w, const float & a) -> Vector;
-
-template<typename BufferT, typename MeshT, typename LoopBodyT, typename VectorT>
-auto UDerivation(const BufferT & u, const MeshT & mesh, LoopBodyT bodyObj, const VectorT & Iion, const BufferT & lumpedM, const float & sigma) -> Vector;
-
-template<typename BufferT>
-auto WDerivation(const BufferT & u, const BufferT & w, const float & b, const float & eps) -> Vector;
+template<typename BufferT, typename MeshT, typename LoopBodyT>
+void computeIionUDerivWDeriv(BufferT & f, BufferT & u_deriv, BufferT & w_deriv, const MeshT & mesh, LoopBodyT & body,
+                             const BufferT & u, const BufferT & w, const BufferT & lumpedM, const float & sigma,
+                             const float & a, const float & b, const float & eps);
 
 /*----------------------------------------------------------------- MAIN --------------------------------------------------------------------------------------*/
 int main(int argc, char** argv)
@@ -78,24 +80,18 @@ int main(int argc, char** argv)
     const Mesh mesh      = Mesh::template CreateFromFile<HPM::auxiliary::AmiraMeshFileReader, ::HPM::mesh::MetisPartitioner>
                            (meshFile, {hpm.GetL1PartitionNumber(), hpm.GetL2PartitionNumber()}, hpm.gaspi_runtime.rank().get());
 
-    /*------------------------------------------(2) Set start values ------------------------------------------------------------------------------------------*/
+    /*------------------------------------------(2) Set directory-,folder- and filename of result -------------------------------------------------------------*/
+    char buff[FILENAME_MAX]; //create string buffer to hold path
+    GetCurrentDir(buff, FILENAME_MAX);
+    std::string currentWorkingDir(buff);
 
-    // input options for small mesh 5x5 (config.cfg -> mesh2D_test5x5.am)
-//    int numIt = 2000; // number of iterations
-//    float h   = 0.015; // step size
-//    float a = -0.1; float b = 0.008; float eps = 0.1; float sigma = -0.1;
-//    float v1 = 0.1; float v2 = 0.1; // value for start vector(s)
+    std::string foldername = "Test_onlyBuffer";
+    std::string filename   = "test_100x100Mesh_onlyBuffer";
 
-    // value option for bigger mesh 100x100 (config.cfg -> mesh2D.am)
-    int numIt = 1500; // number of iterations
-    float h   = 0.2; // step size
-    float a = -0.1; float b = 1e-4; float eps = 0.005; float sigma = -0.1;
-    float v1 = 1; float v2 = 0; // value for start vector(s)
-
-
-
-    float u0L  = v1;  float u0R  = 0.F; // values for start vector u on \Omega_1 and \Omega_2
-    float w0L  = 0.F; float w0R  = v2;  // values for start vector w on \Omega_1 and \Omega_2
+    /*------------------------------------------(3) Set start values ------------------------------------------------------------------------------------------*/
+    int numIt = 1000;
+    float h; float a; float b; float eps; float sigma; float u0L; float u0R; float w0L; float w0R;
+    SetStartValues(2, h, a, b, eps, sigma, u0L, u0R, w0L, w0R);
 
     int numNodes = mesh.template GetNumEntities<0>();
     int maxX = std::ceil(std::sqrt(numNodes)/4);
@@ -107,34 +103,31 @@ int main(int argc, char** argv)
     HPM::Buffer<float, Mesh, Dofs<1, 0, 0, 0>> w(mesh);
     CreateStartVector(mesh, w, w0L, w0R, maxX, maxY, body);
 
-    Vector u_deriv;
-    Vector w_deriv;
-    Vector f_i;
+    HPM::Buffer<float, Mesh, Dofs<1, 0, 0, 0>> u_deriv(mesh);
+    HPM::Buffer<float, Mesh, Dofs<1, 0, 0, 0>> w_deriv(mesh);
+    HPM::Buffer<float, Mesh, Dofs<1, 0, 0, 0>> f(mesh);
 
-    /*------------------------------------------(3) Create monodomain problem ---------------------------------------------------------------------------------*/
+    /*------------------------------------------(4) Create monodomain problem ---------------------------------------------------------------------------------*/
     HPM::Buffer<float, Mesh, Dofs<1, 0, 0, 0>> lumpedMat(mesh);
     AssembleLumpedMassMatrix(mesh, body, lumpedMat);
 
-    // check if startvector was set correctly
-    std::stringstream s;
-    s << 0;
-    std::string name = "test_100x100Mesh_newFWEuler" + s.str();
-    writeVTKOutput2DTime(mesh, name, u, "resultU");
+    // check if startvector was set correctly by creating output file at time step zero
+    std::stringstream s; s << 0;
+    std::string name = filename + s.str();
+    writeVTKOutput2DTime(mesh, currentWorkingDir, foldername, name, u, "resultU");
 
+    // compute
     for (int j = 0; j < numIt; ++j)
     {
-        f_i     = Iion(u, w, a);
-        u_deriv = UDerivation(u, mesh, body, f_i, lumpedMat, sigma);
-        w_deriv = WDerivation(u, w, b, eps);
+        computeIionUDerivWDeriv(f, u_deriv, w_deriv, mesh, body, u, w, lumpedMat, sigma, a, b, eps);
         FWEuler(u, u_deriv, h, body, mesh);
         FWEuler(w, w_deriv, h, body, mesh);
 
         if ((j+1)%10 == 0)
         {
-            std::stringstream s;
-            s << j+1;
-            std::string name = "test_100x100Mesh_newFWEuler" + s.str();
-            writeVTKOutput2DTime(mesh, name, u, "resultU");
+            std::stringstream s; s << j+1;
+            name = filename + s.str();
+            writeVTKOutput2DTime(mesh, currentWorkingDir, foldername, name, u, "resultU");
         }
     }
 
@@ -143,15 +136,53 @@ int main(int argc, char** argv)
 #endif
 
 /*----------------------------------------------------------------- (A) Functions (Implementation): -----------------------------------------------------------*/
+
+//!
+//! \brief Set start settings.
+//!
+void SetStartValues(int option, float& h, float& a, float& b, float& eps, float& sigma, float& u0L, float& u0R, float& w0L, float& w0R)
+{
+    if (option == 1)
+    {
+        // input options for small mesh 5x5 (config.cfg -> mesh2D_test5x5.am)
+        h     = 0.015; // step size
+        a     = -0.1;
+        b     = 0.008;
+        eps   = 0.1;
+        sigma = -0.1;
+        u0L   = 0.1; // values for start vector u on \Omega_1 and \Omega_2
+        u0R   = 0.F; // values for start vector u on \Omega_1 and \Omega_2
+        w0L   = 0.F;  // values for start vector w on \Omega_1 and \Omega_2
+        w0R   = 0.1;  // values for start vector w on \Omega_1 and \Omega_2
+    }
+    else if (option == 2)
+    {
+        // input options for bigger mesh 100x100 (config.cfg -> mesh2D.am)
+        h     = 0.4; // step size
+        a     = -0.1;
+        b     = 1e-4;
+        eps   = 0.005;
+        sigma = -0.1;
+        u0L   = 1.F; // values for start vector u on \Omega_1 and \Omega_2
+        u0R   = 0.F; // values for start vector u on \Omega_1 and \Omega_2
+        w0L   = 0.F;  // values for start vector w on \Omega_1 and \Omega_2
+        w0R   = 0.F;  // values for start vector w on \Omega_1 and \Omega_2
+    }
+    else
+        printf("There is no option choosen for start value settings. Create your own option or choose one of the existing.");
+
+    return;
+}
+
 //!
 //! \brief Create start vector.
 //!
 template<typename MeshT, typename BufferT, typename LoopBodyT>
-void CreateStartVector(const MeshT & mesh, BufferT & startVec, const float & startValLeft, const float & startValRight, const int & maxX, const int & maxY, LoopBodyT bodyObj)
+void CreateStartVector(const MeshT & mesh, BufferT & startVec, const float & startValLeft, const float & startValRight, const int & maxX, const int & maxY, LoopBodyT & body)
 {
     auto nodes { mesh.template GetEntityRange<0>() };
 
-    bodyObj.Execute(HPM::ForEachEntity(
+    body.Execute(HPM::ForEachEntity(
                   nodes,
                   std::tuple(Read(Node(startVec))),
                   [&](auto const& node, const auto& iter, auto& lvs)
@@ -170,10 +201,10 @@ void CreateStartVector(const MeshT & mesh, BufferT & startVec, const float & sta
 //! \brief Assemble rom-sum lumped mass matrix
 //!
 template<typename MeshT, typename LoopBodyT, typename BufferT>
-void AssembleLumpedMassMatrix(const MeshT & mesh, LoopBodyT bodyObj, BufferT & lumpedMat)
+void AssembleLumpedMassMatrix(const MeshT & mesh, LoopBodyT & body, BufferT & lumpedMat)
 {
     auto cells {mesh.template GetEntityRange<2>()};
-    bodyObj.Execute(HPM::ForEachEntity(
+    body.Execute(HPM::ForEachEntity(
                         cells,
                         std::tuple(ReadWrite(Node(lumpedMat))),
                         [&](auto const& cell, const auto& iter, auto& lvs)
@@ -201,10 +232,10 @@ void AssembleLumpedMassMatrix(const MeshT & mesh, LoopBodyT bodyObj, BufferT & l
 //! matrix-vector product split into single scalar operations
 //!
 template<typename MeshT, typename VectorT, typename LoopbodyT, typename BufferT>
-void AssembleMatrixVecProduct2D(const MeshT & mesh, const VectorT & d, LoopbodyT bodyObj, BufferT & sBuffer)
+void AssembleMatrixVecProduct2D(const MeshT & mesh, const VectorT & d, LoopbodyT & body, BufferT & sBuffer)
 {
     auto cells { mesh.template GetEntityRange<2>() };
-    bodyObj.Execute(HPM::ForEachEntity(
+    body.Execute(HPM::ForEachEntity(
                   cells,
                   std::tuple(ReadWrite(Node(sBuffer))),
                   [&](auto const& cell, const auto& iter, auto& lvs)
@@ -251,56 +282,40 @@ void AssembleMatrixVecProduct2D(const MeshT & mesh, const VectorT & d, LoopbodyT
 //! \brief Forward (explicit) Euler algorithm.
 //!
 template<typename BufferT, typename VectorT, typename LoopbodyT, typename MeshT>
-void FWEuler(BufferT & vecOld, const VectorT & vecDeriv, const float & h, LoopbodyT body, const MeshT & mesh)
+void FWEuler(BufferT & vecOld, const VectorT & vecDeriv, const float & h, LoopbodyT & body, const MeshT & mesh)
 {
     auto vertices {mesh.template GetEntityRange<0>()};
     body.Execute(HPM::ForEachEntity(
                   vertices,
                   std::tuple(ReadWrite(Node(vecOld))),
                   [&](auto const& vertex, const auto& iter, auto& lvs)
-    { vecOld[vertex.GetTopology().GetIndex()] += h*vecDeriv[vertex.GetTopology().GetIndex()]; }));
+    {
+        vecOld[vertex.GetTopology().GetIndex()] += h*vecDeriv[vertex.GetTopology().GetIndex()];
+    }));
     return;
 }
 
 //!
-//! \brief Compute ion current at time step t.
+//! \brief Compute ion current, derivation of u and derivation of w at time step t.
 //!
-template<typename BufferT>
-auto Iion(const BufferT & u, const BufferT & w, const float & a) -> Vector
-{
-    Vector f_i; f_i.resize(u.GetSize());
-    for (int i = 0; i < u.GetSize(); ++i)
-        f_i[i] = (u[i] * (1-u[i]) * (u[i]-a)) - w[i];
-
-    return f_i;
-}
-
-//!
-//! \brief Compute derivation of u at time step t.
-//!
-template<typename BufferT, typename MeshT, typename LoopBodyT, typename VectorT>
-auto UDerivation(const BufferT & u, const MeshT & mesh, LoopBodyT bodyObj, const VectorT & Iion, const BufferT & lumpedM, const float & sigma) -> Vector
+template<typename BufferT, typename MeshT, typename LoopBodyT>
+void computeIionUDerivWDeriv(BufferT & f, BufferT & u_deriv, BufferT & w_deriv, const MeshT & mesh, LoopBodyT & body,
+                             const BufferT & u, const BufferT & w, const BufferT & lumpedM, const float & sigma,
+                             const float & a, const float & b, const float & eps)
 {
     HPM::Buffer<float, Mesh, Dofs<1, 0, 0, 0>> s(mesh);
-    AssembleMatrixVecProduct2D(mesh, u, bodyObj, s);
+    AssembleMatrixVecProduct2D(mesh, u, body, s);
 
-    Vector u_deriv; u_deriv.resize(u.GetSize());
+    auto vertices {mesh.template GetEntityRange<0>()};
+    body.Execute(HPM::ForEachEntity(vertices, std::tuple(
+                                    ReadWrite(Node(f)),/*Read*/Write(Node(u_deriv)),/*Read*/Write(Node(w_deriv))),
+                                    [&](auto const& vertex, const auto& iter, auto& lvs)
+    {
+        int id      = vertex.GetTopology().GetIndex();
+        f[id]       = (u[id] * (1-u[id]) * (u[id]-a)) - w[id];
+        u_deriv[id] = ((1/lumpedM[id]) * sigma * s[id]) + f[id];
+        w_deriv[id] = eps*(u[id]-b*w[id]);
+    }));
 
-    for (int i = 0; i < u.GetSize(); ++i)
-        u_deriv[i] += ((1/lumpedM[i]) * sigma * s[i]) + Iion[i];
-
-    return u_deriv;
-}
-
-//!
-//! \brief Compute derivation of w at time step t.
-//!
-template<typename BufferT>
-auto WDerivation(const BufferT & u, const BufferT & w, const float & b, const float & eps) -> Vector
-{
-    Vector w_deriv; w_deriv.resize(w.GetSize());
-    for (int i = 0; i < w.GetSize(); ++i)
-        w_deriv[i] = eps*(u[i]-b*w[i]);
-
-    return w_deriv;
+    return;
 }
